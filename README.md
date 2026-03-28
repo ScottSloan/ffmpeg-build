d07cb12099d73b6fdcfdff9feaac847c10110a40
ScottSloan <world1019@sina.com>
2026-03-28 22:43:56 +0800

    ci: 测试 macOS 构建

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ffmpeg-build
 Bili23 Downloader 定制版 ffmpeg
+
+## 编译流程
+### 先编译 libmp3lame
+```bash
+./configure \
+--prefix=/d/FFmpeg/lame-3.100/build \
+--disable-shared \
+--disable-frontend \
+--enable-static \
+```
+
+### 再编译 ffmpeg
+参考 `build.sh`
---
+++ build.sh
@@ -0,0 +1,31 @@
+export ARCH=x86_64
+
+./configure \
+--prefix=/home/scott/build/opt/ffmpeg \
+--disable-doc \
+--disable-shared \
+--disable-everything \
+--disable-programs \
+--disable-swscale \
+--disable-filters \
+--disable-swresample \
+--disable-avx512 \
+--disable-network \
+--disable-avdevice \
+--disable-autodetect \
+--enable-demuxer='concat,ffmetadata,mov,mp4,flv,m4a,mp3,m4a' \
+--enable-muxer='mp4,flv,mp3,m4a,flac' \
+--enable-decoder='h264,hevc,av1,aac,flac,eac3,ac3,mjpeg,png,webp' \
+--enable-encoder='libmp3lame,flac' \
+--enable-parser='mjpeg' \
+--enable-static \
+--enable-small \
+--enable-ffmpeg \
+--enable-protocol='file,concat' \
+--enable-zlib \
+--enable-libmp3lame \
+--enable-gpl \
+--extra-ldflags='-static -static-libgcc -static-libstdc++' \
+--extra-ldflags="-L/home/scott/build/opt/lame-3.100/lib" \
+--extra-cflags="-I/home/scott/build/opt/lame-3.100/include" \
+

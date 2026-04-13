c5528a733e9a2236fd48fdb9b1b01401b877dcf2
ScottSloan <world1019@sina.com>
2026-04-13 22:57:41 +0800

    fix: 修复缺少 mjpeg 解码器的问题

--- build.sh
+++ build.sh
@@ -13,11 +13,13 @@ export ARCH=x86_64
 --disable-network \
 --disable-avdevice \
 --disable-autodetect \
---enable-demuxer='concat,ffmetadata,mov,mp4,flv,m4a,mp3,matroska' \
+--enable-demuxer='concat,ffmetadata,mov,mp4,flv,m4a,mp3,matroska,image2' \
 --enable-muxer='mp4,flv,mp3,m4a,flac,matroska' \
 --enable-decoder='h264,hevc,av1,aac,flac,eac3,ac3,mjpeg,png,webp' \
---enable-encoder='libmp3lame,flac' \
+--enable-encoder='libmp3lame,flac,mjpeg' \
 --enable-parser='mjpeg,h264,hevc,av1,aac,flac,ac3,eac3' \
+--enable-swscale \
+--enable-filter=scale \
 --enable-static \
 --enable-small \
 --enable-ffmpeg \
